385c07c26f787424251939a9c43067054e0d1426
Marcos Henrique <marcoshf2008@gmail.com>
2026-02-27 18:41:59 -0300

    Adiciona conexão ao BANCO

---
+++ DB/conexao.js
@@ -0,0 +1,22 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  databese: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect((err) => {
+  if (err) {
+    console.log("Error ao conectar");
+    console.log("MOTIVO: ", err.message);
+  } else {
+    console.log("Conecatdo!");
+  }
+});
+
+export default Pool;
